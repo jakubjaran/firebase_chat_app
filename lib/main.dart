e9684a15b7954f56507df8cff160c6fcfd5b6ce8
unknown <jakubjaran@gmail.com>
2021-03-09 10:53:10 +0100

    Testing firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import './screens/chat_screen.dart';
 
@@ -7,6 +8,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp();
     return MaterialApp(
       title: 'Chat App',
       theme: ThemeData(
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (ctx, i) => Text('TEST'),
         itemCount: 5,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/QeLTMEDt6rL08KR7pJmG/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((doc) {
+              print(doc['text']);
+            });
+          });
+        },
       ),
     );
   }
